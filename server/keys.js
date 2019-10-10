7e35bfe9df89c046de2a619392c3e20e1e98a57b
Nick Brown <nickrossbrown@gmail.com>
2019-10-09 21:25:24 -0600

    add very simple express api

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDISHOST,
+    redisPort: process.env.REDISPORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.envPGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
